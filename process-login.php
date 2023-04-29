47932a6bfc4d0f2c03dc4c542b9f1a9def22c483
“DavsIsaiah” <jidava@mymail.mapua.edu.ph>
2023-04-29 16:03:00 +0800

    rename

--- process-login.php
+++ process-login.php
@@ -3,7 +3,7 @@ if (session_id() == "") {
     session_start();
 }
 require __DIR__ . '/vendor/autoload.php';
-include('Connection.php');
+include('connection.php');
 $ref_table = "cred";
 if (isset($_POST['submit'])) {
     $user = $_POST['user'];
